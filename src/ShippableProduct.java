e6560721d6455ae1487a4b4a6e5769586f8be030
gehadepada <gehadepada@gmail.com>
2025-07-05 15:39:20 +0300

    feature:implement ShippableProduct class

--- src/ShippableProduct.java
+++ src/ShippableProduct.java
@@ -1,3 +1,21 @@
-public class ShippableProduct extends Product{
+public class ShippableProduct extends Product implements Shippable{
     double weightInKg;
+    ShippableProduct(String name , double balance , int quantity ,double weightInKg)
+    {
+        super(name , balance ,  quantity);
+        this.weightInKg=weightInKg;
+    }
+
+    @Override
+    public double getWeight() {
+        return weightInKg;
+    }
+
+    @Override
+    public String getName() {
+        return name;
+    }
+
+
+
 }
