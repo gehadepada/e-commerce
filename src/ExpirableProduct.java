7ce4c12b4bb1d5888361e277bbca8d1218a91fd1
gehadepada <gehadepada@gmail.com>
2025-07-05 15:33:23 +0300

    feature:implement ExpirableProduct class

--- src/ExpirableProduct.java
+++ src/ExpirableProduct.java
@@ -1,5 +1,16 @@
 import java.util.Date;
 
-public class ExpirableProduct extends Product{
-    Date expiryDate ;
+public class ExpirableProduct extends Product implements Expirable {
+    Date expiryDate;
+
+    ExpirableProduct(String name, double price, int quantity, Date expiryDate) {
+        super(name, price, quantity);
+        this.expiryDate = expiryDate;
+
+    }
+
+    @Override
+    public boolean isExpired() {
+        return new Date().after(expiryDate);
+    }
 }
--- src/Product.java
+++ src/Product.java
@@ -1,5 +1,12 @@
 public class Product {
-    String name ;
-    double price ;
+    String name;
+    double price;
     int quantity;
+
+    Product(String name, double price, int quantity) {
+        this.name = name;
+        this.price=price;
+        this.quantity=quantity;
+
+    }
 }
